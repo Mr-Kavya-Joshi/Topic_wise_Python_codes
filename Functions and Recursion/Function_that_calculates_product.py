e93a6ee966dae378221ab268dd45476475699575
Kavya Joshi <emailkavya03@gmail.com>
2026-05-13 17:28:15 +0530

    Create Function_that_calculates_product.py

---
+++ Functions and Recursion/Function_that_calculates_product.py
@@ -0,0 +1,7 @@
+#This function calculates the product of two numbers
+def prod(a,b):
+  prod=a*b
+  return prod
+int1 = int(input("Enter the first integer : "))
+int2 = int(input("Enter the second integer : "))
+print("The product is : ",prod(int1,int2))
