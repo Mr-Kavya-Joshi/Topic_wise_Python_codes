d12e9ae7885a4b82a6502bafcc0c0a3f1c49f66c
Kavya Joshi <emailkavya03@gmail.com>
2026-05-13 17:33:30 +0530

    Update Function_that_calculates_product.py

--- Functions and Recursion/Function_that_calculates_product.py
+++ Functions and Recursion/Function_that_calculates_product.py
@@ -2,6 +2,17 @@
 def prod(a,b):
   prod=a*b
   return prod
+def withoutPrintProd(a,b):
+  prod=a*b
+  print(prod)
+  return prod
 int1 = int(input("Enter the first integer : "))
 int2 = int(input("Enter the second integer : "))
-print("The product is : ",prod(int1,int2))
+
+print("The product is : ")
+
+#The first function would need an additional print command
+print(prod(int1,int2))
+
+#The second function simply needs to be called
+withoutPrintProd(int1,int2)
